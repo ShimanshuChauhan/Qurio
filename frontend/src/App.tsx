87163fedef04ba25cb38cf498887d461c582ee29
ShimanshuChauhan <shivanshuchauhan699@gmail.com>
2025-07-21 22:42:23 +0530

    feat : added quiz page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import MainLayout from "./layouts/MainLayout"
 import PageNotFound from "./components/PageNotFound"
 import Home from "./pages/Home"
+import Quiz from "./pages/Quiz"
 
 function App() {
   return (
@@ -9,9 +10,10 @@ function App() {
       <Route path="/" element={<MainLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<div>About</div>} />
+        <Route path="quiz" element={<Quiz />} />
         <Route path="*" element={<PageNotFound />} />
       </Route>
-    </Routes>
+    </Routes >
   )
 }
 
